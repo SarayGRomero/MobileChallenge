dcad4a7a2a465374f062e515822def68f10f533c
Saray Gallardo <sargalrom@gmail.com>
2024-05-21 19:49:18 +0200

    Initial commit

---
+++ app/src/main/java/com/cabify/cabifymobilechallengexml/presentation/NavHostActivity.kt
@@ -0,0 +1,12 @@
+package com.cabify.cabifymobilechallengexml.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.cabify.cabifymobilechallengexml.R
+
+class NavHostActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nav_host)
+    }
+}
